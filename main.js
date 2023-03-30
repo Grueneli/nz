b9279ba2f2b518f0acda9fc76b04f9ec7aa75fd6
LisaGrüner <elisabeth.gruener@freenet.de>
2023-03-30 15:46:29 +0200

    Variabeln eingefügt

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ let stop_lat = -43.53;
 let stop_lng = 170.15;
 
 
-let map = L.map('map').setView([-43.53, 170.15], 13);
+let map = L.map('map').setView([stop_lat, stop_lng], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
  }).addTo(map);
 
-L.marker([-43.53, 170.15]).addTo(map)
+L.marker([stop_lat, stop_lng]).addTo(map)
  .bindPopup('Der Fox Gletscher')
 .openPopup();
