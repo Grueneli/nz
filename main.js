34b6e0b480ea647f5f0613690fbdf4eb8636eaa9
LisaGrüner <elisabeth.gruener@freenet.de>
2023-03-30 15:50:37 +0200

    Variabeln Zoom und Variabeln Titel eingefügt

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
 let stop_lat = -43.53;
 let stop_lng = 170.15;
+let zoom = 13; // mit 13 wird angegeben wie weit reingezoomt werden soll => Faktor 13
+let title= 'Der Fox Gletscher'
 
-
-let map = L.map('map').setView([stop_lat, stop_lng], 13);
+let map = L.map('map').setView([stop_lat, stop_lng], zoom);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
  }).addTo(map);
 
 L.marker([stop_lat, stop_lng]).addTo(map)
- .bindPopup('Der Fox Gletscher')
+ .bindPopup(title)
 .openPopup();
