ace4987852c57726c25f07e34449b786708cd2a1
LisaGrüner <elisabeth.gruener@freenet.de>
2023-03-30 16:17:55 +0200

    Const STOP eingefügt

--- main.js
+++ main.js
@@ -3,12 +3,14 @@ let stop_lng = 170.15;
 let zoom = 13; // mit 13 wird angegeben wie weit reingezoomt werden soll => Faktor 13
 let title= 'Der Fox Gletscher'
 
-const STOPS = ["Wanaka","Der Fox Gletscher", "Franz-Josef Gletscher"];  //Konstante definiert, groß geschrieben, da man sie nicht mehr überschreiben kann
+const STOPS = ["Wanaka",{
+    title:"Der Fox Gletscher",
+     user: "grueneli", 
+     lat: -43.53, 
+     lng: 170.15, 
+     Wikipedia: "https://de.wikipedia.org/wiki/Fox-Gletscher" //solang bei dem Objekt Namen nur strings vorhanden sind, braucht man keine ""
+}, "Franz-Josef Gletscher"];  //Konstante definiert, groß geschrieben, da man sie nicht mehr überschreiben kann
 console.log (STOPS);
-console.log (STOPS [0]);
-console.log (STOPS [1]);
-console.log (STOPS [2]);
-console.log (STOPS.length);
 
 let map = L.map('map').setView([stop_lat, stop_lng], zoom);
 
@@ -19,3 +21,5 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([stop_lat, stop_lng]).addTo(map)
  .bindPopup(title)
 .openPopup();
+
+//lat, lng, Wikipedia
