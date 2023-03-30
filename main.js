673d1299c0c8ddd43fce6e9eaea02895697a09bc
LisaGrüner <elisabeth.gruener@freenet.de>
2023-03-30 17:57:20 +0200

    Maßstab hinzufügen

--- main.js
+++ main.js
@@ -99,6 +99,7 @@ console.log (STOPS);
 
 
 let map = L.map('map').setView([stop_lat, stop_lng], zoom);
+L.control.scale({imperial: false}).addTo(map); //Maßstab hinzufügen. Wenns nicht sein soll, muss der Wert gleich Falsch gesetzt werden
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
