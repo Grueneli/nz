10e495c77834e450a6c22062cbbea5a28bb34ddb
LisaGrüner <elisabeth.gruener@freenet.de>
2023-04-19 11:21:39 +0200

    roads rückgängig

--- main.js
+++ main.js
@@ -108,17 +108,11 @@ let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
  }).addTo(map);
 let watercolor = L.tileLayer.provider('Stamen.Watercolor').addTo(map);
-var roads = L.gridLayer
-	.googleMutant({
-		type: "roadmap", // valid values are 'roadmap', 'satellite', 'terrain' and 'hybrid'
-	})
-	.addTo(map);
 
 
 L.control.layers({
     "Openstreetmap":osm,
     "Watercolor": watercolor,
-    "roadmap": roads
 }).addTo(map)
 
 for (let stop of STOPS){
